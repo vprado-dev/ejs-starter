1efb95921725d257936a148092e995f71446661f
Vinicius Prado <vinicavagna@gmail.com>
2021-10-26 14:57:23 -0300

    :hammer: small refactor

--- src/endpoints/index.ts
+++ src/endpoints/index.ts
@@ -1,5 +1,5 @@
 import { endpoint } from "@ev-fns/endpoint";
 
 export const index = endpoint(async (req, res) => {
-  res.render("index", { name: process.env.API_NAME });
+  res.render("index");
 });
